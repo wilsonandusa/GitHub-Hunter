55d8bdd7c73792d18c7bbe98ec9bd794db533bda
wilsonandiphone@icloud.com <wilsonandiphone@icloud.com>
2017-07-05 00:34:33 -0400

    improve mid column section
    - implemented the parser
    - added css

--- src/parsing.js
+++ src/parsing.js
@@ -1,7 +1,8 @@
 var Crawler = require('crawler')
+var shell = require('electron').shell
 
 // document.getElementById('programming-languages-btn').onclick =
-// crawlingOneUrl('https://github.com/showcases/programming-languages')
+crawlingOneUrl('https://github.com/showcases/programming-languages')
 
 
 function crawlingOneUrl (url) {
@@ -21,8 +22,8 @@ function crawlingOneUrl (url) {
         var reposLang = []
         var $ = res.$
         $('.repo-list-item-with-avatar').each(function (i, elem) {
-          reposNames[i] = $(this).find('h3').find('a').find('span').text().replace(/\s/g, '')
-          reposLinks[i] = 'https://github.com' + $(this).find('h3').find('a').text().replace(/\s/g, '')
+          reposNames[i] = $(this).find('h3').find('a').text().replace(/\s/g, '')
+          reposLinks[i] = 'https://github.com/' + $(this).find('h3').find('a').text().replace(/\s/g, '')
           reposImg[i] = $(this).find('img').attr('src')
           reposInfo[i] = $(this).find('div').find('div').text()
           $(this).find('div > a').each(function (index, element) {
@@ -42,11 +43,50 @@ function crawlingOneUrl (url) {
         })
 
         for (var i = 0; i < reposNames.length; i++) {
-          // reposNames[i]
+          // set up info rows
+          var topRow = document.createElement('div')
+          var buttonRow = document.createElement('div')
+          topRow.className = 'row'
+          buttonRow.className = 'row'
+          topRow.style.margin = '5px'
+          // set up repos two info rows
+          var rightDiv = document.createElement('div')
+          rightDiv.style.marginLeft = '5px'
+          var nameBar = document.createElement('div')
+          nameBar.className = 'middle_box_info_row_name'
+          var starForkBar = document.createElement('div')
+          starForkBar.className = 'middle_box_info_row_star_fork'
+
+
+          // setup repos Name
+          var name = document.createElement('a')
+          name.innerHTML = reposNames[i]
+          name.id = i
+          name.addEventListener('click', function () {
+            shell.openExternal(reposLinks[this.id])
+          })
+          nameBar.appendChild(name)
+          starForkBar.innerHTML = 'Star: ' + reposStar[i] +
+           ' ' + 'Fork:' + reposFork[i]
+          rightDiv.appendChild(nameBar)
+          rightDiv.appendChild(starForkBar)
+
           var boxList = document.getElementById('showcase')
           var box = document.createElement('div')
           box.className = 'middle_button_box'
-          // setUp
+
+          // setUp icon
+          var boxIcon = document.createElement('div')
+          boxIcon.className = 'middle_box_icon'
+          boxIcon.style.backgroundImage = "url('" + reposImg[i] + "')"
+
+          topRow.appendChild(boxIcon)
+          topRow.appendChild(rightDiv)
+
+          box.appendChild(topRow)
+          box.appendChild(buttonRow)
+
+          // append box elements
           boxList.appendChild(box)
         }
         // console.log(reposNames)
@@ -66,23 +106,3 @@ function crawlingOneUrl (url) {
     url
   ])
 }
-
-function crawlingUrls (urlArray) {
-  var c = new Crawler({
-    maxConnections: 10,
-    callback: function (error, res, done) {
-      if (error) {
-        console.log(error)
-      } else {
-        var $ = res.$
-        console.log($('title').text())
-      }
-      done()
-    }
-  })
-  c.queue(urlArray)
-}
-// function log() {
-//   console.log('hahahahha')
-// }
-// module.exports = log;
